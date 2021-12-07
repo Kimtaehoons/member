461b5b30634a610c649aee97f015af7480fa980f
sharpboy77 <sharpboy77@naver.com>
2021-12-07 09:08:08 +0900

    member 업로드

---
+++ static/js/main_js.js
@@ -0,0 +1,11 @@
+//그림에 마우스 이벤트 효과
+var pic = document.getElementById('pic');
+pic.onmouseover = changePic; //함수 호출 ()'괄호'생략
+pic.onmouseout = originPic;
+
+function changePic(){ //사진변경
+    pic.src = "../static/images/healing.jpg";
+}
+function originPic(){ //원래 사진으로 복원
+    pic.src = "../static/images/activity.jpg";
+}
