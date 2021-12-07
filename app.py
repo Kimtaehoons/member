b9bac793caf25f914337a8fbe0178541d2db77ac
sharpboy77 <sharpboy77@naver.com>
2021-12-07 12:51:06 +0900

    member 업로드

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
+import sqlite3
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+def getconn():
+    conn = sqlite3.connect('./memberdb.db')
+    return conn
+
 @app.route('/') #127.0.0.1:/5000
 def index():
     return render_template('index.html')
+
+@app.route('/memberlist/')
+def memberlist():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM member"
+    cur.execute(sql)
+    rs = cur.fetchall() #db에서 검색한 데이터
+    conn.close()
+    return render_template('memberlist.html', rs=rs) #가져온 데이터를 웹으로 보내줌
+
+@app.route('/member_view/<string:id>/')
+def member_view(id): #mid를 경로로 설정하고 매개변수로 넘겨줌
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM member WHERE mid = '%s' " % (id)
+    cur.execute(sql)
+    rs = cur.fetchone()
+    conn.close()
+    return render_template('member_view.html', rs=rs)
 
 app.run(debug=True)
---
+++ tbl_member.py
@@ -0,0 +1,48 @@
+#미리 안 만들고 여기서 db 만들기
+import sqlite3
+
+def getconn():
+    conn = sqlite3.connect('./memberdb.db') #같은 곳에 db생성
+    return conn
+
+def create_table():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = """
+    CREATE TABLE member(
+        mid CHAR(5) PRIMARY KEY,
+        passwd CHAR(8) NOT NULL,
+        name TEXT NOT NULL,
+        age INTEGER,
+        regDate DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+    #regDate는 파이참의 현재날짜와 같이 자동생성되는 것
+    cur.execute(sql)
+    conn.commit()
+    print("member테이블 생성") #호출 하기 전에 생성해서 파이참 콘솔창에서 확인 용도
+    conn.close()
+
+def insert_member():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "INSERT INTO member (mid, passwd, name, age) VALUES (?, ?, ?, ?)" #regDate는 자동생성되는 것으로 입력제외 처리(나머지 것만 입력되도록 설정)
+    cur.execute(sql, ('10002', 'm1234', '팥쥐', 19)) #id는 문자로 하는 것이 처리하는데 오류가 줄어듦
+    conn.commit()
+    print("멤버 추가")
+    conn.close()
+
+def select_member(): #db브라우저에서 확인 안 하고자
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM member"
+    cur.execute(sql)
+    rs = cur.fetchall() #db에서 반환된 자료
+    print(rs) #리스트형태로 출력
+    for i in rs:
+        print(i[0]) #인덱싱을 통한 자료 정렬
+    conn.close() #commit이 필요없음
+
+#create_table() #호출, 테이블 생성된 것을 db브라우저에서 확인 가능, 처음할 때만 열고 다음부터는 막아준다
+#insert_member()
+select_member()
